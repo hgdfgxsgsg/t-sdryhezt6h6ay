7e68208c50275f00c7820bbbee28e32287061112
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-11 19:23:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2499,8 +2499,9 @@ client.on('message', message => {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond: اضغط هنا `)
-        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=466256531998900246&permissions=8&scope=bot`)
+        .setTitle(`:small_orange_diamond: هنا `)
+        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=484518205495443476&permissions=8&scope=bot
+`)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
        }
@@ -3339,6 +3340,32 @@ client.on("guildMemberAdd", (member) => {
 
 
 
+
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+الســلام عليكم ورحمة الله وبركاته :heart:
+مرحباً بك في سيرفرنا
+**
+    `)
+
+}).catch(console.error)
+
+})
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+    **
+    الســلام عليكم ورحمة الله وبركاته :heart:
+    مرحباً بك في سيرفرنا
+    **
+    `);
+
+  }).catch(console.error)
+
+})
 
 
 
