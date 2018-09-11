394a1cec56558a603c6b445a6c41b4f676c938a6
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-11 14:42:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3229,5 +3229,72 @@ channel.guild.owner.send(`<@!${channelremover.id}>
     message.channel.send(id)
 }       });
 
-// THIS  MUST  BE  THIS  WAY
+
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "1bc")) {
+    let i = client.users.size;
+    if(message.author.id !== '252813587188416512') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
