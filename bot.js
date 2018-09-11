fcf38f437cb0f429b8f3ffd5e91f3978a7fd40d5
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-11 19:24:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2499,7 +2499,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond: هنا `)
+        .setTitle(`:small_orange_diamond: انه طغضا `)
         .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=484518205495443476&permissions=8&scope=bot
 `)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
