dfa2f4dbf77fd5b0aa9da76388ce36576acac94d
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-11 15:08:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2621,7 +2621,7 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 if(message.content.startsWith(prefix + "daily")) {
   if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
-    profile[message.author.id].credits += 100000000000000000000000000000
+    profile[message.author.id].credits += 300
      message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
