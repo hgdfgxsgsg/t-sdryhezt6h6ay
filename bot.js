0c8242807492974e340452a5fae463b59e89ad9b
mohamed502 <40800726+mohamed502@users.noreply.github.com>
2018-09-12 16:37:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2837,7 +2837,7 @@ message.channel.stopTyping()
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/TZ3dcyC**")
+  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/BUbjb9M**")
      
      
   message.channel.sendEmbed(embed);
